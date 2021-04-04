0fde7c83ed1570c5137397f2db3019dae745a386
Sardorkhon <t.sardor1706@gmail.com>
2021-04-04 16:31:25 +0500

    Final

--- webpage/index.php
+++ webpage/index.php
@@ -40,14 +40,13 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Validating Forms</title>
-		<link href="style.css" type="text/css" rel="stylesheet" />
-	</head>
-	
-	<body>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Validating Forms</title>
+	<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
 		<h1>Registration Form</h1>
 
 		<p>
@@ -61,87 +60,112 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 		<hr />
 
 		<form action="index.php" method="POST">
+		
 		<dl>
+
 			<dt>Name</dt>
 			<dd>
 				<input type="text" name="name" required>
 			</dd>
+
+			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
+
 			<dt>Email</dt>
 			<dd>
 				<input type="text" name="email" required>
 			</dd>
+
 			<dt>Username</dt>
 			<dd>
 				<input type="text" name="username" required>
 			</dd>
+
 			<dt>Password</dt>
 			<dd>
 				<input type="password" name="password" required>
 			</dd>
+			
 			<dt>Confirm Password</dt>
 			<dd>
 				<input type="password" name="confirmPassword" required>
 			</dd>
+			
 			<dt>Date of Birth</dt>
 			<dd>
 				<input type="text" name="dateOfBirth" required>
 			</dd>
+			
 			<dt>Gender</dt>
 			<dd>
 				<input type="text" name="gender" required>
 			</dd>
+			
 			<dt>Marital Status</dt>
 			<dd>
 				<input type="text" name="maritalStatus" required>
 			</dd>
+			
 			<dt>Address</dt>
 			<dd>
 				<input type="text" name="address" required>
 			</dd>
+			
 			<dt>City</dt>
 			<dd>
 				<input type="text" name="city" required>
 			</dd>
+			
 			<dt>Postal Code</dt>
 			<dd>
 				<input type="text" name="postalCode" required>
 			</dd>
+			
 			<dt>Home Phone</dt>
 			<dd>
 				<input type="text" name="homePhone" required>
 			</dd>
+			
 			<dt>Mobile Phone</dt>
 			<dd>
 				<input type="text" name="mobilePhone" required>
 			</dd>
+			
 			<dt>Credit Card Number</dt>
 			<dd>
 				<input type="text" name="cardNumber" required>
 			</dd>
+			
 			<dt>Credit Card Expiry Date</dt>
 			<dd>
 				<input type="text" name="expiryDate" required>
-			</dd><dt>Monthly Salary</dt>
+			</dd>
+
+			<dt>Monthly Salary</dt>
 			<dd>
 				<input type="text" name="monthlySalary" required>
 			</dd>
+			
 			<dt>Web Site URL</dt>
 			<dd>
 				<input type="text" name="url" required>
-			</dd><dt>Overall GPA</dt>
+			</dd>
+
+			<dt>Overall GPA</dt>
 			<dd>
 				<input type="text" name="gpa" required>
 			</dd>
 
-			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
 		</dl>
 
 		<div>
 			<input type="submit" value="SEND">
 		</div>
+		
 		</form>
+
 		<?php if($_SERVER["REQUEST_METHOD"]=="POST"){?>
 		<h1><?php echo $out;?></h1>
 		<?php } ?>
-	</body>
+
+</body>
 </html>
